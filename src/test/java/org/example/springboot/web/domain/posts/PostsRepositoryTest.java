48c76670806b324406d42f118192d01100dde1ea
bgr <jamasa97@icloud.com>
2022-02-10 00:49:42 +0900

    Test: Dto, Controller, Posts 기능 구현

---
+++ src/test/java/org/example/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,48 @@
+package org.example.springboot.web.domain.posts;
+
+import org.example.springboot.domain.posts.Posts;
+import org.example.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "test content";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jamasa97@icloud.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo("테스트 게시글");
+        assertThat(posts.getContent()).isEqualTo("test content");
+
+    }
+}
